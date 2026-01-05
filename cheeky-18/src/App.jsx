ff38e3538198e96b61a74211aaccc4a04d92b66d
satwikchandra <satwikchandra65@gmail.com>
2026-01-05 10:33:26 +0530

    yo

---
+++ cheeky-18/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/Home.jsx'
+import Loading from './components/Loading.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Loading />
+    </div>
+  )
+}
+
+export default App
